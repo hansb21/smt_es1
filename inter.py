30252d8495b9ec338efe26d2dc41d347667bbc1f
Hans Buss <hans;buss@mailfence.com>
2022-05-05 20:09:23 -0300

    main window finished

--- inter.py
+++ inter.py
@@ -10,15 +10,15 @@ class PlayerInterface:
 
     def fill_main_window(self):
         # Título, ícone, dimensionamento e fundo da janela
-        self.main_window.title("Shin Megami Tem 6")
+        self.main_window.title("Shin Megami Tem 6 - Press Turn Battle")
         self.main_window.geometry("1280x720")
-        self.main_window.resizable(False, False)
-        self.main_window["bg"]="DarkOrchid2"
+        self.main_window.resizable(True, True)
+        self.main_window["bg"]="gray"
         # Criação de 2 frames e organização da janela em um grid de 2 linhas e 1 coluna,
         # sendo que table_frame ocupa a linha superior e message_frame, a inferior
-        self.table_frame = Frame(self.main_window, padx=100, pady=25, bg="DarkOrchid2")
+        self.table_frame = Frame(self.main_window, padx=100, pady=25, bg="gray")
         self.table_frame.grid(row=0 , column=0)
-        self.message_frame = Frame(self.main_window, padx=0, pady=10, bg="DarkOrchid2")
+        self.message_frame = Frame(self.main_window, padx=0, pady=10, bg="gray")
         self.message_frame.grid(row=1 , column=0)
         self.an_image = PhotoImage(file="images/battle_fill.png") #pyimage1
         self.logo = PhotoImage(file="images/smt_logo.png") #pyimage2
@@ -30,9 +30,27 @@ class PlayerInterface:
             #    aLabel.grid(row=x , column=y)
              #   a_column.append(aLabel)
         #self.board_view.append(a_column)
-        self.logo_label = Label(self.message_frame, bd = 0, image=self.logo)
-        self.logo_label.grid(row=0, column=0)
-        self.message_label = Label(self.message_frame, bg="gold3", text=' Gobblet Gobblers', font="arial 30")
-        self.message_label.grid(row=0, column=1)
+        self.logo_label = Label(self.table_frame, bd = 0, image=self.logo)
+        self.logo_label.grid(row=0, column=3)
+        self.message_label = Label(self.message_frame, bg="gray", text=' Shin Megami Tem 6 - Press Turn Battle', font="arial 15")
+        self.message_label.grid(row=2, column=6)
+        
+        self.startb = Button(self.message_frame,
+                           text="Começar",
+                           command=self.clear_frame)
+        self.endb = Button(self.message_frame,
+                            text="Sair",
+                            command=lambda:exit())
+        self.startb.grid(row=3, column=6)
+        self.endb.grid(row=4, column=6)
 
+    def print_hello(self):
+        print("hello world")
+
+    def clear_frame(self):
+        for widget in self.table_frame.winfo_children():
+            widget.destroy()
+
+        for widget in self.message_frame.winfo_children():
+             widget.destroy()
 PlayerInterface()
