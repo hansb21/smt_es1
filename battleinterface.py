aa63f99fd4172b6cc2ed5ef6713e3314381e93f2
Júlio Ramos <juliogoncalvesramos@yahoo.com.br>
2022-07-15 15:11:43 -0300

    Interface gráfica refeita

--- battleinterface.py
+++ battleinterface.py
@@ -1,5 +1,39 @@
 from tkinter import *
 from turtle import width
+from typing import TextIO
+
+class DisplayCell:
+    def __init__(self, parent, width, height, frameBorder, frameText, textOnly):
+        self.parent = parent
+        self.width = width
+        self.height = height
+        self.frameBorder = frameBorder
+        self.frameText = frameText
+        self.textOnly = textOnly
+
+        self.frame = LabelFrame(self.parent, bd=self.frameBorder, text=self.frameText)
+        self.imagem = Label(self.frame, text="Imagem")
+        self.text = Text(self.frame, state='disabled')
+
+    def place(self,x,y):
+        self.frame.place(x=x, y=y, height=self.height, width=self.width)
+        if self.textOnly:
+            self.text.place(relheight=1, relwidth=1, relx=0, rely=0)
+        else:
+            self.imagem.place(relheight=0.75, relwidth=1, relx=0, rely=0)
+            self.text.place(relheight=0.25, relwidth=1, relx=0, rely=0.75)
+        print("placed")
+
+    def insertText(self, texto):
+        self.text.configure(state="normal")
+        self.text.insert("0.0", texto)
+        self.text.configure(state="disabled")
+
+    def replaceTest(self, pos, texto):
+        self.text.configure(state="normal")
+        end = pos + " lineend"
+        self.text.replace(pos, end, texto)
+        self.text.configure(state="disabled")
 
 class BattleInterface:
     def __init__(self):
@@ -10,11 +44,20 @@ class BattleInterface:
 
         #Definindo elementos
         self.defineFrames()
-        self.defineButtons()
-        self.defineLabelFrames()
-        self.defineLabels()
+        self.defineMainButtons()
+        self.defineTurnosCell()
+        self.definePlayerCells()
+        self.defineEnemyCells()
 
-        self.fill_main_window()
+        #self.fill_main_window()
+        self.placeFrames()
+        self.placeMainButtons()
+        self.placeTurnosCell()
+        self.placePlayerCells()
+        self.placeEnemyCells()
+
+        self.formatEntityCells()
+
         self.main_window.mainloop()
 
     def defineFrames(self):
@@ -22,12 +65,17 @@ class BattleInterface:
         self.field_frame = Frame(self.main_window, bg="green", width=1152, height=528)
         self.entity_frame = Frame(self.main_window, bg="blue", width=1280, height=192)
 
-    def defineButtons(self):
+    def placeFrames(self):
+        self.menu_frame.place(x=0, y=0)
+        self.field_frame.place(x=128, y=0)
+        self.entity_frame.place(x=0, y=528)
+
+    def defineMainButtons(self):
         self.ataque_button = Button(self.menu_frame ,bg="grey99", text="Ataque", command=lambda:self.unplaceAll())
         self.item_button = Button(self.menu_frame ,bg="grey99", text="Itens", command=lambda:self.debug_button("Uso de item"))
         self.fundir_button = Button(self.menu_frame ,bg="grey99", text="Fusão", command=lambda:self.debug_button("Ritual de fusão") )
         self.invocar_button = Button(self.menu_frame ,bg="grey99", text="Invocar", command=lambda:self.debug_button("Ritual de invocação"))
-        self.passar_button = Button(self.menu_frame ,bg="grey99", text="Passar Turno", command=lambda:self.debug_button("Passagem de turno"))
+        self.passar_button = Button(self.menu_frame ,bg="grey99", text="Passar Turno", command=lambda:self.replaceTest())
 
         self.lista_botoes = [   self.ataque_button, 
                                 self.item_button, 
@@ -35,140 +83,66 @@ class BattleInterface:
                                 self.invocar_button,
                                 self.passar_button]
 
-    def defineLabelFrames(self):
-        #LABELFRAME DOS TURNOS
-        self.turnos_frame = LabelFrame(self.menu_frame, bd=5, text="Turnos")
-
-        #LABELFRAME DA ENTIDADE ATUAL
-        self.entidade_atual_frame = LabelFrame(self.field_frame, bd=5, text="Entidade atual")
-
-        #LABELFRAMES DOS INIMIGOS
-        #Usar como pai dos labels dos inimigos
-        self.inimigo1_frame = LabelFrame(self.field_frame, bd=5, text="Inimigo 1")
-        self.inimigo2_frame = LabelFrame(self.field_frame, bd=5, text="Inimigo 2")
-        self.inimigo3_frame = LabelFrame(self.field_frame, bd=5, text="Inimigo 3")
-        self.inimigo4_frame = LabelFrame(self.field_frame, bd=5, text="Inimigo 4")
-        self.lista_inimigo_frames = [   self.inimigo1_frame, 
-                                        self.inimigo2_frame, 
-                                        self.inimigo3_frame, 
-                                        self.inimigo4_frame]
-
-        #LABELFRAMES DO TIME
-        #Usar como pai dos labels do time
-        #Campo
-        self.campo1_frame = LabelFrame(self.entity_frame, bd=5, text="Campo 1")
-        self.campo2_frame = LabelFrame(self.entity_frame, bd=5, text="Campo 2")
-        self.campo3_frame = LabelFrame(self.entity_frame, bd=5, text="Campo 3")
-        self.campo4_frame = LabelFrame(self.entity_frame, bd=5, text="Campo 4")
-        self.lista_time_frames = [  self.campo1_frame, 
-                                    self.campo2_frame, 
-                                    self.campo3_frame, 
-                                    self.campo4_frame]
-        #Reserva
-        self.reserva1_frame = LabelFrame(self.entity_frame, bd=5, text="Reserva 1")
-        self.reserva2_frame = LabelFrame(self.entity_frame, bd=5, text="Reserva 2")
-        self.reserva3_frame = LabelFrame(self.entity_frame, bd=5, text="Reserva 3")
-        self.reserva4_frame = LabelFrame(self.entity_frame, bd=5, text="Reserva 4")
-        self.reserva5_frame = LabelFrame(self.entity_frame, bd=5, text="Reserva 5")
-        self.reserva6_frame = LabelFrame(self.entity_frame, bd=5, text="Reserva 6")
-        self.reserva7_frame = LabelFrame(self.entity_frame, bd=5, text="Reserva 7")
-        self.reserva8_frame = LabelFrame(self.entity_frame, bd=5, text="Reserva 8")
-        self.lista_reserva_frames = [   self.reserva1_frame, 
-                                        self.reserva2_frame, 
-                                        self.reserva3_frame, 
-                                        self.reserva4_frame, 
-                                        self.reserva5_frame, 
-                                        self.reserva6_frame, 
-                                        self.reserva7_frame, 
-                                        self.reserva8_frame]
-
-    def defineLabels(self):
-        #LABEL DOS TURNOS
-        self.turnos_label = Label(self.turnos_frame, text="5")
-
-        #Entidade atual
-        self.entidade_atual_label = Label(self.entidade_atual_frame, text="Teste")
-
-        #LABELS DOS INIMIGOS
-        self.inimigo1_label = Label(self.inimigo1_frame, text="Imagem 1")
-        self.inimigo2_label = Label(self.inimigo2_frame, text="Imagem 2")
-        self.inimigo3_label = Label(self.inimigo3_frame, text="Imagem 3")
-        self.inimigo4_label = Label(self.inimigo4_frame, text="Imagem 4")
-        self.lista_inimigo_labels = [   self.inimigo1_label, 
-                                        self.inimigo2_label, 
-                                        self.inimigo3_label, 
-                                        self.inimigo4_label]
-
-        #LABELS DO TIME
-        #Campo
-        self.campo1_label = Label(self.campo1_frame, text="Imagem 1")
-        self.campo2_label = Label(self.campo2_frame, text="Imagem 2")
-        self.campo3_label = Label(self.campo3_frame, text="Imagem 3")
-        self.campo4_label = Label(self.campo4_frame, text="Imagem 4")
-        self.lista_campo_labels = [  self.campo1_label,
-                                    self.campo2_label,
-                                    self.campo3_label,
-                                    self.campo4_label]
-        #Reserva
-        self.reserva1_label = Label(self.reserva1_frame, text="Imagem 1")
-        self.reserva2_label = Label(self.reserva2_frame, text="Imagem 2")
-        self.reserva3_label = Label(self.reserva3_frame, text="Imagem 3")
-        self.reserva4_label = Label(self.reserva4_frame, text="Imagem 4")
-        self.reserva5_label = Label(self.reserva5_frame, text="Imagem 1")
-        self.reserva6_label = Label(self.reserva6_frame, text="Imagem 2")
-        self.reserva7_label = Label(self.reserva7_frame, text="Imagem 3")
-        self.reserva8_label = Label(self.reserva8_frame, text="Imagem 4")
-        self.lista_reserva_labels = [  self.reserva1_label,
-                                    self.reserva2_label,
-                                    self.reserva3_label,
-                                    self.reserva4_label,
-                                    self.reserva5_label,
-                                    self.reserva6_label,
-                                    self.reserva7_label,
-                                    self.reserva8_label]
-
-
-    def fill_main_window(self):
-
-        self.menu_frame.place(x=0, y=0)
-
-        self.field_frame.place(x=128, y=0)
-
-        self.entity_frame.place(x=0, y=528)
-
+    def placeMainButtons(self):
         for i, botao in enumerate(self.lista_botoes):
-            calc_pos=55*i
+            calc_pos=75+55*i
             botao.place(x=0, y=calc_pos, width=128, height=55)
 
-        self.turnos_frame.place(x=0,y=275, width=128,height=55)
+    def defineTurnosCell(self):
+        self.turnosCell = DisplayCell(self.menu_frame, 128, 75, 5, "Turnos", True)
 
-        self.turnos_label.place(relheight=1, relwidth=1)
+    def placeTurnosCell(self):
+        self.turnosCell.place(0,0)
 
-        self.entidade_atual_frame.place(x=0, y=0,width=230, height=528)
+    def definePlayerCells(self):
+        self.campoCells = []
+        for i in range(4):
+            texto = "Campo {}".format(i)
+            newCell = DisplayCell(self.entity_frame, 320, 96, 5, texto, True)
+            self.campoCells.append(newCell)
 
-        self.entidade_atual_label.place(x=0, y=0,width=230, height=528)
+        self.reservaCells = []
+        for i in range(4):
+            texto = "Reserva {}".format(i)
+            newCell = DisplayCell(self.entity_frame, 320, 96, 5, texto, True)
+            self.reservaCells.append(newCell)
 
-        for i, inimigoFrame in enumerate(self.lista_inimigo_frames):
-            calc_pos=230*i+230
-            inimigoFrame.place(x=calc_pos,y=0,width=230,height=528)
+            self.atualCell = DisplayCell(self.field_frame, 230, 528, 5, "Entidade Atual", False)
 
-        for inimigoLabel in self.lista_inimigo_labels:
-            inimigoLabel.place(relheight=1, relwidth=1)
-
-        for i, campoFrame in enumerate(self.lista_time_frames):
+    def placePlayerCells(self):
+        for i, cell in enumerate(self.campoCells):
             calc_pos=320*i
-            campoFrame.place(x=calc_pos,y=0,width=320,height=96)
-       
-        for i, reservaFrame in enumerate(self.lista_reserva_frames):
-            calc_pos=160*i
-            reservaFrame.place(x=calc_pos,y=96,width=160,height=96)
+            cell.place(calc_pos, 0)
 
-        for campoLabel in self.lista_campo_labels:
-            campoLabel.place(relheight=1, relwidth=1)
+        for i, cell in enumerate(self.reservaCells):
+            calc_pos=320*i
+            cell.place(calc_pos, 96)
 
-        for reservaLabel in self.lista_reserva_labels:
-            reservaLabel.place(relheight=1, relwidth=1)
-        
+        self.atualCell.place(0, 0)
+
+    def defineEnemyCells(self):
+        self.enemyCells = []
+        for i in range(4):
+            texto = "Inimigo {}".format(i)
+            newCell = DisplayCell(self.field_frame, 230, 528, 5, texto, False)
+            self.enemyCells.append(newCell)
+
+    def placeEnemyCells(self):
+        for i, cell in enumerate(self.enemyCells):
+            calc_pos=230*i+230
+            cell.place(calc_pos, 0)
+
+    def formatEntityCells(self):
+        for (campo, reserva, inimigo) in zip(self.campoCells, self.reservaCells, self.enemyCells):
+            campo.insertText("NOME\nHP=100\nMP=100")
+            reserva.insertText("NOME\nHP=100\nMP=100")
+            inimigo.insertText("NOME\nHP=100\nMP=100")
+
+        self.atualCell.insertText("NOME\nHP=100\nMP=100")
+        self.turnosCell.insertText("10")
+
+    def replaceTest(self):
+        self.campoCells[0].replaceTest("2.0", "REPLACE")
 
     def debug_button(self, escolha):
         print(f"{escolha} realizado sucesso")
