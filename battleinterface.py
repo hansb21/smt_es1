2b3688ca65daf48f6c02ad8de9d39f2bb3493182
Hans Buss <hans;buss@mailfence.com>
2022-07-24 18:36:54 -0300

    能了能了

--- battleinterface.py
+++ battleinterface.py
@@ -12,7 +12,7 @@ from displaycell import DisplayCell
 class BattleInterface:
     def __init__(self):
         self.main_window = Tk()
-        self.main_window.title("Press Turn Battle")
+        self.main_window.title("Shin Megami Tem 6 - Press Turn Battle")
         self.main_window.geometry("1280x720")
         self.main_window.resizable(FALSE,FALSE)
 
@@ -29,7 +29,8 @@ class BattleInterface:
         self.fusao1 = None
         self.fusao2 = None
 
-        self.iniciar()
+        self.fill_main_window()
+        #self.iniciar()
         
         #self.telaFinal()
 
@@ -581,3 +582,76 @@ class BattleInterface:
         novamente.place(relx=0.45, rely=0.45, relwidth=0.1, relheight=0.1)
         mudar.place(relx=0.45, rely=0.60, relwidth=0.1, relheight=0.1)
         sair.place(relx=0.45, rely=0.75, relwidth=0.1, relheight=0.1)
+    
+    def fill_main_window(self):
+        # Título, ícone, dimensionamento e fundo da janela
+        self.main_window.title("Shin Megami Tem 6 - Press Turn Battle")
+        self.main_window.geometry("1280x720")
+        self.main_window.resizable(True, True)
+        self.main_window["bg"]="gray"
+        # Criação de 2 frames e organização da janela em um grid de 2 linhas e 1 coluna,
+        # sendo que table_frame ocupa a linha superior e message_frame, a inferior
+        self.table_frame = Frame(self.main_window, padx=100, pady=25, bg="gray")
+        self.table_frame.grid(row=0 , column=0)
+        self.message_frame = Frame(self.main_window, padx=0, pady=10, bg="gray")
+        self.message_frame.grid(row=1 , column=0)
+        self.an_image = PhotoImage(file="images/battle_fill.png") #pyimage1
+        self.logo = PhotoImage(file="images/smt_logo.png") #pyimage2
+        self.board_view=[]
+        #aLabel = Label(self.table_frame, bd = 0, image=self.an_image)
+
+        self.logo_label = Label(self.table_frame, bd = 0, image=self.logo)
+        self.logo_label.grid(row=0, column=3)
+        self.message_label = Label(self.message_frame, bg="gray", text=' Shin Megami Tem 6 - Press Turn Battle', font="arial 15")
+        self.message_label.grid(row=2, column=6)
+        
+        self.startb = Button(self.message_frame,
+                           text="Começar",
+                           command=self.fill_character)
+        self.endb = Button(self.message_frame,
+                            text="Sair",
+                            command=lambda:self.main_window.destroy)
+        self.startb.grid(row=3, column=6)
+        self.endb.grid(row=4, column=6)
+
+    def fill_character(self):
+        
+        self.table_frame.destroy()
+        self.message_frame.destroy()
+        self.table_frame = Frame(self.main_window, padx=100, pady=25, bg="gray")
+        self.table_frame.grid(row=0 , column=0)
+        self.message_frame = Frame(self.main_window, padx=0, pady=10, bg="gray")
+        self.message_frame.grid(row=1 , column=0)
+        
+        p1 = Label(self.table_frame, text= "Player 1",  width=15, font=("Arial", 25))
+        p2 = Label(self.table_frame, text= "Player 2", width=15, font=("Arial", 25))
+        p1.grid(row = 0, column = 0)
+        p2.grid(row = 0, column = 80)
+        
+        self.humano1 = PhotoImage(file='images/sprites/humanos/humano1.png')
+        self.humano2 = PhotoImage(file='images/sprites/humanos/humano2.png')
+        p1b = Button(self.message_frame, text="PLAYER 1", font=("Arial",25), height=150, width=58, image=self.humano1)
+        p2b = Button(self.message_frame, text="PLAYER 2", font=("Arial",25), height=150, width=58, image=self.humano2)
+        p1b.grid(row=1, column=0)
+        p2b.grid(row=1, column=80)
+        
+        confirmarbp1 = IntVar()
+        confirmarbp2 = IntVar()
+
+        confirm_bp1 = Checkbutton(self.message_frame, font=("Arial", 10),  text="CONFIRM", bg="gray", height=2, width=15, variable=confirmarbp1, onvalue=1, offvalue=0)
+        confirm_bp2 = Checkbutton(self.message_frame, font=("Arial", 10),  text="CONFIRM", bg="gray", height=2, width=15, variable=confirmarbp2, onvalue=1, offvalue=0)
+        confirm_bp1.grid(row=2, column=0)
+        confirm_bp2.grid(row=2, column=80)
+        confirm_button = Button(self.message_frame, text='BATALHAR!', command=lambda: self.confirmaTela(confirmarbp1, confirmarbp2))
+        confirm_button.grid(row=3, column=70)
+        
+    
+    def confirmaTela(self, confirmarbp1, confirmarbp2):
+        if (confirmarbp1.get() == 1 and confirmarbp2.get() == 1):
+            self.iniciar()
+
+    def clear_frame(self):
+        for widget in self.table_frame.place_slaves():
+                widget.place_forget()
+        for widget in self.message_frame.place_slaves():
+                widget.place_forget()
